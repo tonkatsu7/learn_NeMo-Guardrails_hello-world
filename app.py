c3a685ca2e244e5b6e3b170ac892b6dac055b8f2
tonkatsu7 <sipham@gmail.com>
2024-08-31 16:26:28 +1000

    first guardrail

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from nemoguardrails import RailsConfig, LLMRails
 
 config = RailsConfig.from_path("./config")
 
-print("type of config: ", type(config))
-print("config=", config)
+# print("type of config: ", type(config))
+# print("config=", config)
 
 rails = LLMRails(config)
 
 response = rails.generate(messages=[{
   "role": "user",
-  "content": "Hello, how are you?"
+  "content": "Hello! How are you??"
 }])
 
 print("response=", response)
+print("\n\n")
+
+print("response=", response['content'])
